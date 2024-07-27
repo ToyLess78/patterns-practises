5b693a0d3ed25d6d7e05579a84bbaa3d75b69a6a
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-07-27 13:42:09 +0300

    feat: update services client

---
+++ client/src/services/card.service.ts
@@ -0,0 +1,41 @@
+import { socket } from '../context/socket';
+import { CardEvent } from '../common/enums/enums';
+import { Card } from '../common/types/card.type';
+
+let timeoutId: NodeJS.Timeout;
+
+export const handleRenameCard = (listId: string, cardId: string, newName: string) => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    newName && socket.emit(CardEvent.RENAME, listId, cardId, newName);
+  }, 500);
+};
+
+export const handleDeleteCard = (listId: string, cardId: string) => {
+  socket.emit(CardEvent.DELETE, listId, cardId);
+};
+
+export const handleDuplicate = (listId: string, cardId: string) => {
+  socket.emit(CardEvent.DUPLICATE, listId, cardId);
+};
+
+export const handleCreateCard = (listId: string, name: string) => {
+  (name.trim() !== '') && socket.emit(CardEvent.CREATE, listId, name);
+};
+
+export const changeDescription = (listId: string, cardId: string, value: string) => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    value && socket.emit(CardEvent.CHANGE_DESCRIPTION, listId, cardId, value);
+  }, 500);
+};
+
+export const addCardToList = (cards: Card[], index: number, card: Card): Card[] => {
+  cards.splice(index, 0, card);
+  return cards;
+};
+
+export const removeCardFromList = (cards: Card[], index: number): Card[] => {
+  cards.splice(index, 1);
+  return cards;
+};
---
+++ client/src/services/list.service.ts
@@ -0,0 +1,41 @@
+import { socket } from '../context/socket';
+import { Card } from '../common/types/card.type';
+import { ListEvent } from '../common/enums/enums';
+import { List } from '../common/types/list.type';
+
+export const removeCardFromList = (cards: Card[], index: number): Card[] =>
+  cards.slice(0, index).concat(cards.slice(index + 1));
+
+export const addCardToList = (cards: Card[], index: number, card: Card): Card[] =>
+  cards.slice(0, index).concat(card).concat(cards.slice(index));
+
+export const handleCreateList = (name: string) => {
+ (name.trim() !== '') ? socket.emit(ListEvent.CREATE, name) : null;
+};
+
+export const handleDeleteList = (listId: string) => {
+    socket.emit(ListEvent.DELETE, listId);
+};
+
+let timeoutId: NodeJS.Timeout;
+
+export const handleRenameList = (listId: string, newName: string) => {
+  clearTimeout(timeoutId);
+  timeoutId = setTimeout(() => {
+    newName && socket.emit(ListEvent.RENAME, listId, newName);
+  }, 500);
+};
+
+export const updateListCards = (lists: List[], listId: string, updateFn: (cards: Card[]) => Card[]): List[] =>
+  lists.map(list => list.id === listId ? { ...list, cards: updateFn([...list.cards]) } : list);
+
+export const getCardsFromList = (lists: List[], listId: string): Card[] => {
+  const list = lists.find(list => list.id === listId);
+  return list ? list.cards : [];
+};
+
+export const reorderArray = <T>(array: T[], startIndex: number, endIndex: number): T[] => {
+  const [removed] = array.splice(startIndex, 1);
+  array.splice(endIndex, 0, removed);
+  return array;
+};
