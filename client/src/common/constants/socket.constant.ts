fa0ccbe187af8e9aa73614746ad4fbdc0c291e21
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-07-28 13:32:14 +0300

    fix: update variable

--- client/src/common/constants/socket.constant.ts
+++ client/src/common/constants/socket.constant.ts
@@ -1,4 +1,4 @@
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:3005';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://patterns-practises-production.up.railway.app/';
 
 export { SOCKET_URL };
 
