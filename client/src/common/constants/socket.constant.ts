dc4dfad936c92b56090a01b3fd1ce55b2c2856c9
Tetiana Bilenko <toyless@gmail.com>
2025-01-23 23:04:47 +0200

    Update socket.constant.ts

--- client/src/common/constants/socket.constant.ts
+++ client/src/common/constants/socket.constant.ts
@@ -1,4 +1,4 @@
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://patterns-practises-production.up.railway.app/';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'https://test-chat.up.railway.app/';
 
 export { SOCKET_URL };
 
